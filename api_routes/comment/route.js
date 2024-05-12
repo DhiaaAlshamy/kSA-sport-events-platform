8e0b3e68140a56acb516b9646af07010e4100051
Dhiaa Alshamy <dhodhof@gmail.com>
2024-05-13 01:33:35 +0300

    all api routes

---
+++ api_routes/comment/route.js
@@ -0,0 +1,66 @@
+
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+export async function GET(request) {
+  try {
+    const items = await prisma.comment.findMany();
+    return new Response(JSON.stringify(items), {
+      headers: { "Content-Type": "application/json" },
+      status: 200,
+    });
+  } catch (error) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      status: 500,
+    });
+  }
+}
+
+export async function POST(request) {
+  try {
+    const itemData = await request.json();
+    const item = await prisma.comment.create({
+      data: itemData,
+    });
+    return new Response(JSON.stringify(item), {
+      headers: { "Content-Type": "application/json" },
+      status: 201,
+    });
+  } catch (error) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      status: 500,
+    });
+  }
+}
+
+export async function PUT(request) {
+  try {
+    const { id, ...data } = await request.json();
+    const item = await prisma.comment.update({
+      where: { id },
+      data,
+    });
+    return new Response(JSON.stringify(item), {
+      headers: { "Content-Type": "application/json" },
+      status: 200,
+    });
+  } catch (error) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      status: 500,
+    });
+  }
+}
+
+export async function DELETE(request) {
+  try {
+    const { id } = await request.json();
+    await prisma.comment.delete({
+      where: { id },
+    });
+    return new Response(null, { status: 204 });
+  } catch (error) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      status: 500,
+    });
+  }
+}
---
+++ app/api/club/route.js
@@ -0,0 +1,66 @@
+
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+export async function GET(request) {
+  try {
+    const items = await prisma.club.findMany();
+    return new Response(JSON.stringify(items), {
+      headers: { "Content-Type": "application/json" },
+      status: 200,
+    });
+  } catch (error) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      status: 500,
+    });
+  }
+}
+
+export async function POST(request) {
+  try {
+    const itemData = await request.json();
+    const item = await prisma.club.create({
+      data: itemData,
+    });
+    return new Response(JSON.stringify(item), {
+      headers: { "Content-Type": "application/json" },
+      status: 201,
+    });
+  } catch (error) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      status: 500,
+    });
+  }
+}
+
+export async function PUT(request) {
+  try {
+    const { id, ...data } = await request.json();
+    const item = await prisma.club.update({
+      where: { id },
+      data,
+    });
+    return new Response(JSON.stringify(item), {
+      headers: { "Content-Type": "application/json" },
+      status: 200,
+    });
+  } catch (error) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      status: 500,
+    });
+  }
+}
+
+export async function DELETE(request) {
+  try {
+    const { id } = await request.json();
+    await prisma.club.delete({
+      where: { id },
+    });
+    return new Response(null, { status: 204 });
+  } catch (error) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      status: 500,
+    });
+  }
+}
